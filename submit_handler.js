ec38ebd67197fda5e354ec3543424b4c220f7faf
triyanshagr-arch <atriyanshagr@gmail.com>
2026-06-18 12:02:26 +0530

    Update Google Apps Script URL for Approved members tab

--- submit_handler.js
+++ submit_handler.js
@@ -124,7 +124,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const signatureUrl = applicantSignatureInput.files.length > 0 ? await readFileAsDataURL(applicantSignatureInput.files[0]) : null;
 
                 // Send data to Google Sheets in the background
-                const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzQotNsxXhilECQP917rcet7hKLxEXJZdA5Om1P2M6rVq2F3PuKZfaZ7FxopWKJUee/exec";
+                const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx3hHUVv-gYDO4411ozdvQ97M7loKuubQwFjFIMj_S-QoL7dIH6hQzUG4BZ6aWjMoSI/exec";
                 fetch(GOOGLE_SCRIPT_URL, {
                     method: 'POST',
                     body: JSON.stringify({ action: 'add_membership', data: { membershipNo: membershipNo, photoBase64: compressedPhotoBase64, ...dataObj } })
