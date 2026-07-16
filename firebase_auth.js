f34c1939e20b6da12b36a5077c972a860236c0f4
triyanshagr-arch <atriyanshagr@gmail.com>
2026-07-16 19:32:55 +0530

    Initialize Firebase and create modern login.html UI

---
+++ firebase_auth.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9nRwYG-geFQnAR2vL9sSoGwJy8UtOE58",
+    authDomain: "agrawal-samaj-samiti-website.firebaseapp.com",
+    projectId: "agrawal-samaj-samiti-website",
+    storageBucket: "agrawal-samaj-samiti-website.firebasestorage.app",
+    messagingSenderId: "953083660315",
+    appId: "1:953083660315:web:80c4c93cf81688f9e150be",
+    measurementId: "G-FNH488K5ZF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.useDeviceLanguage();
+
+export { auth, RecaptchaVerifier, signInWithPhoneNumber, onAuthStateChanged, signOut };
