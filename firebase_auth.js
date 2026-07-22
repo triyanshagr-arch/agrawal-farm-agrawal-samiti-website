6c77d9d70e2fd6f3ff8ce5d2a92d47b62503c56d
triyanshagr-arch <atriyanshagr@gmail.com>
2026-07-22 12:23:18 +0530

    Force Firebase Auth SMS messages to be sent in Hindi

--- firebase_auth.js
+++ firebase_auth.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-auth.useDeviceLanguage();
+auth.languageCode = 'hi';
 
 // Explicitly force local persistence so it survives across tabs
 setPersistence(auth, browserLocalPersistence).catch((error) => {
